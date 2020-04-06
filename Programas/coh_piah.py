e8739586bded5c927c5251951179035194acd8d8
João Vinaud <leyenasd@gmail.com>
2020-04-06 17:41:57 -0300

    implemenntaçao da avaliação de textos

--- Programas/coh_piah.py
+++ Programas/coh_piah.py
@@ -218,7 +218,7 @@ def calcula_assinatura(texto):
     return assinatura
 
 def compara_assinatura(as_a, as_b):
-    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver
+    '''Essa funcao recebe duas assinaturas de texto e deve devolver
      o grau de similaridade nas assinaturas. A função de comparação é:
      S(ab) = (Somatório(i=1 -> 6) |F i,a - F i,b|) / 6, onde:
      S(ab) - grau de similardade entre duas assinatura;
@@ -239,7 +239,13 @@ def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos
      e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade
       de ter sido infectado por COH-PIAH.'''
-    pass
+
+    numero_maior = 0
+    for i in range(len(textos)):
+        assinatura = calcula_assinatura(textos[i])
+        if compara_assinatura(assinatura,ass_cp) > numero_maior:
+            numero_maior = i
+    return numero_maior
 
 def main():
     #recebe a assinatura ja conhecida de uma pessoa específica
@@ -264,6 +270,14 @@ def test_run_compara_assinatura():
     valor = compara_assinatura([4.34, 0.05, 0.02, 12.81, 2.16, 0.0],[3.96, 0.05, 0.02, 22.22, 3.41, 0.0])
     print("valor da comparação:", valor)
 
+def test_run_avalia():
+    Textos = ['Navegadores antigos tinham uma frase gloriosa:"Navegar é preciso; viver não é preciso".Quero para mim o espírito [d]esta frase,transformada a forma para a casar como eu sou:Viver não é necessário; o que é necessário é criar.Não conto gozar a minha vida; nem em gozá-la penso.Só quero torná-la grande,ainda que para isso tenha de ser o meu corpo e a (minha alma) a lenha desse fogo.Só quero torná-la de toda a humanidade;ainda que para isso tenha de a perder como minha.Cada vez mais assim penso.Cada vez mais ponho da essência anímica do meu sangueo propósito impessoal de engrandecer a pátria e contribuirpara a evolução da humanidade.É a forma que em mim tomou o misticismo da nossa Raça.', 'Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.', 'NOSSA alegria diante dum sistema metafisico, nossa satisfação em presença duma construção do pensamento, em que a organização espiritual do mundo se mostra num conjunto lógico, coerente a harmônico, sempre dependem eminentemente da estética; têm a mesma origem que o prazer, que a alta satisfação, sempre serena afinal, que a atividade artística nos proporciona quando cria a ordem e a forma a nos permite abranger com a vista o caos da vida, dando-lhe transparência.']
+    assinatura = [4.79, 0.72, 0.56, 80.5, 2.5, 31.6]
+
+    valor= avalia_textos(Textos, assinatura)
+    print('valor:',valor)
+
 #test_run_assinatura()
-test_run_compara_assinatura()
+#test_run_compara_assinatura()
+test_run_avalia()
 
