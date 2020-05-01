dff4086c2c3029d3e48189fa1480844ad75b7d44
João Vinaud <leyenasd@gmail.com>
2020-05-01 15:24:38 -0300

    exercicio elefantes

---
+++ Recursividade/elefantes.py
@@ -0,0 +1,26 @@
+"""
+Implemente a função incomodam(n) que devolve uma string contendo "incomodam " 
+(a palavra seguida de um espaço) n vezes. Se n não for um inteiro estritamente positivo, 
+a função deve devolver uma string vazia. Essa função deve ser implementada utilizando recursão.
+
+Utilizando a função acima, implemente a função elefantes(n) que devolve uma string contendo 
+a letra da música "Um elefante incomoda muita gente" de 1 até n elefantes. Se n não for maior que 1,
+ a função deve devolver uma string vazia. Essa função também deve ser implementada utilizando recursão.
+"""
+
+def incomodam(n):
+    if n < 1:
+        return ""
+    return "incomodam " + incomodam(n-1)
+
+def elefantes(n):
+    if n < 1:
+        return ""
+    if n == 1:
+        return "Um elefante incomoda muita gente\n"
+    if n == 2:
+        return elefantes(n-1) +str(n)+" elefantes " +incomodam(n)+"muito mais\n" 
+
+    return elefantes(n-1)+str(n-1)+" elefantes incomodam muita gente \n"+str(n)+" elefantes " + incomodam(n)+"muito mais\n" 
+
+print(elefantes(4))
