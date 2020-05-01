bcb1efa9da7578dba4c04dd26c421d62680fc2b3
João Vinaud <leyenasd@gmail.com>
2020-05-01 14:16:17 -0300

    soma recursiva

--- Recursividade/soma_lista_recursivo.py
+++ Recursividade/soma_lista_recursivo.py
@@ -1,9 +1,9 @@
 # soma os elementos de uma lista de forma recursiva
-def soma(lista):
+def soma_lista(lista):
     
     if len(lista) == 0:
         return 0
     else:
-        return lista[0] + soma(lista[1:])
+        return lista[0] + soma_lista(lista[1:])
 
-print(soma([1,2,3,4,5,]))
+print(soma_lista([1,2,3,4,5,]))
