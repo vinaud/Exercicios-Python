62dd83e6a34fac3e0023dd2b683cefccdb9db188
João Vinaud <leyenasd@gmail.com>
2020-05-01 15:28:02 -0300

    ajuste elefantes

--- Recursividade/elefantes.py
+++ Recursividade/elefantes.py
@@ -19,8 +19,8 @@ def elefantes(n):
     if n == 1:
         return "Um elefante incomoda muita gente\n"
     if n == 2:
-        return elefantes(n-1) +str(n)+" elefantes " +incomodam(n)+"muito mais\n" 
+        return elefantes(n-1)+str(n)+" elefantes "+incomodam(n)+"muito mais\n" 
 
-    return elefantes(n-1)+str(n-1)+" elefantes incomodam muita gente \n"+str(n)+" elefantes " + incomodam(n)+"muito mais\n" 
+    return elefantes(n-1)+str(n-1)+" elefantes incomodam muita gente\n"+str(n)+" elefantes " + incomodam(n)+"muito mais\n" 
 
 print(elefantes(4))
